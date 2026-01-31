c1b614e6be1a8788a0fa6d26e9d36b0b8caa4a80
harsh <harshgrwl18@gmail.com>
2026-02-01 01:57:50 +0530

    allow request from all origins

--- src/main/java/com/resumeanalyser/resume_analyser_backend/config/WebConfig.java
+++ src/main/java/com/resumeanalyser/resume_analyser_backend/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174", "http://localhost:5173") // include dev ports you use
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedOriginPatterns("*") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
